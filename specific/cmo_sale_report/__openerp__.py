aef5397c95db18db6b18afb29dcdab75d2b58f70
phongyanon <phongyanon@gmail.com>
2017-06-02 17:10:54 +0700

    [ADD] print report button for sale module

---
+++ specific/cmo_sale_report/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "CMO :: Sale Report Jasper (PDF)",
+    'summary': "",
+    'author': "Ecosoft",
+    'website': "http://ecosoft.co.th",
+    'category': 'Tools',
+    "version": "1.0",
+    'depends': [
+        'sale',
+        'report',
+    ],
+    'data': [
+        'report/report_saleorder.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
