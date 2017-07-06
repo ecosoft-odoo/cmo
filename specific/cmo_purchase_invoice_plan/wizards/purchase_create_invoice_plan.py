e4829f87dbab6650b5778c39c5b4a84a8635dee2
Tharathip <newtratip>
2017-07-06 17:46:38 +0700

    [IMP] remove selection list

--- specific/cmo_purchase_invoice_plan/wizards/purchase_create_invoice_plan.py
+++ specific/cmo_purchase_invoice_plan/wizards/purchase_create_invoice_plan.py
@@ -6,6 +6,5 @@ class PurchaseCreateInvoicePlan(models.Model):
     _inherit = 'purchase.create.invoice.plan'
 
     invoice_mode = fields.Selection(
-        [('change_price', 'As 1 Job (change price)'), ],
         default='change_price',
     )
